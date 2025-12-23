51688212b91c8dc73376a1f0c89bb399b54f0036
Danilo Silveira Ramos <danilosramos2005@gmail.com>
2025-12-22 21:09:39 -0300

    First Steps

--- 01-java-development-fundamentals/src/Ex01.java
+++ 01-java-development-fundamentals/src/Ex01.java
@@ -1,7 +1,7 @@
-public class Main{
+public class Ex01 {
 
-    static void main() {
-        System.out.println("Hello World!")
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
 }
