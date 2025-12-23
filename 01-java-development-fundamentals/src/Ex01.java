365554d1c4be14e09cdadcc3f84def78d49f0daa
Danilo Silveira Ramos <danilosramos2005@gmail.com>
2025-12-22 22:35:22 -0300

    .

--- 01-java-development-fundamentals/src/Ex01.java
+++ 01-java-development-fundamentals/src/Ex01.java
@@ -2,6 +2,7 @@ public class Ex01 {
 
     static void main() {
         System.out.println("Hello World!");
+        String msg = "Programming in Java";
+        System.out.println(msg);
     }
-
 }
