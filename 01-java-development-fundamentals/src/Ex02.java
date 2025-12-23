6ae22e7e680ae21f0cabad5d9f10502816393517
Danilo Silveira Ramos <danilosramos2005@gmail.com>
2025-12-23 16:54:22 -0300

    .

--- 01-java-development-fundamentals/src/Ex02.java
+++ 01-java-development-fundamentals/src/Ex02.java
@@ -1,4 +1,20 @@
-public class Ex02 {
+private final static String WELCOME_MESSAGE = "Hello, type your name:";
 
+void main() {
+
+    //comment
+
+    Scanner scanner = new Scanner(System.in);
+    IO.println(WELCOME_MESSAGE);
+    String name = scanner.next();
+    IO.println("Type your age: ");
+    int age = scanner.nextInt();
+    IO.println("Hello " + name + " your age is " + age);
+    System.out.printf("Hello %s your age is %s \n", name, age);
+
+        /*
+        envelope
+        comment
+        */
 
 }
